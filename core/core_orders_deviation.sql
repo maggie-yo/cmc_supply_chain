e4c7620c2325da0877142943263839d1f3afad72
myyguu <82869398+myyguu@users.noreply.github.com>
2023-05-18 18:55:18 +0200

    Create core_orders_deviation.sql

---
+++ core/core_orders_deviation.sql
@@ -0,0 +1,34 @@
+SELECT
+-- orders
+  o.order_date,
+  -- o.order_delivery_date,
+  o.batch_size,
+  o.num_batches,
+  o.material_number,
+
+ -- suppliers
+  s.supplier_capacity,
+  s.supplier_batch_size,
+
+  -- materials
+  m.measure_unit,
+  m.supplier,
+  m.type,
+
+
+FROM
+  `maggies-sandbox.novo_case.stg_orders` as o
+left join 
+  `maggies-sandbox.novo_case.stg_materials` as m
+  on o.material_number=m.material_number
+
+left join
+  `maggies-sandbox.novo_case.stg_suppliers` as s
+  on m.supplier=s.supplier
+
+where
+-- to find orders where the batch sizes exceed the maximum capacity of produced batches from a supplier
+o.batch_size > s.supplier_batch_size
+
+-- ensuring we always place orders with a positive quantity
+-- and o.batch_size <= 0
